d2ba4bcb26a5e2ff58cb137628312cf835b5477a
Beellee <halley1986n@gmail.com>
2023-07-25 22:19:22 +0200

    projects page and components done

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -1,14 +1,21 @@
 //rfce
 import React from 'react'
 import "../styles/projectDisplay.css"
-import imageUni from "../images/uniPage.png"
 
-function Project({projectTitle}) {
+
+function Project({projectTitle, languages, projectDescription, image}) {
   return (
     <div className='project'>
-      <img className='image' src={imageUni}/>
+      <img className='project-img' src={image}/>
       <h3>{projectTitle}</h3>
-
+      <div className='languages'>
+        {languages.map((language, index) => (
+          <button key={index} className='languageButton'>
+            {language}
+          </button>
+        ))}
+      </div>
+      <p className='project-p'>{projectDescription}</p>
     </div>
   )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,8 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import Project from "../components/ProjectDisplay";
 import '../styles/projects.css';
 
+//imagenes de los proyectos
+import imageUni from "../images/uniPage.png"
+
 function UniProjects() {
   const scrollContainerRef = useRef(null);
+
+  // Languages of the projects
+  const projectUni = ['JavaScript', 'HTML', 'CSS'];
 
   useEffect(() => {
     const scrollContainer = scrollContainerRef.current;
@@ -24,14 +30,23 @@ function UniProjects() {
   return (
     
     <div ref={scrollContainerRef} className='scrollableObject'>
-      <div>
+      <div className='projectsTitle'>
         <h1>Projects</h1>
-        <h3>Exhibition this way ➔</h3>
+        <h3 className='projects-h3'>Exhibition this way ➔</h3>
       </div>
       <div className='projectsWrapper'>
-        <Project
-          projectTitle="A"
-        />
+      <Project 
+        projectTitle="University page" 
+        languages={projectUni}
+        image = {imageUni}
+        projectDescription = "This project serves as a digital repository for all my university projects. The website is built using CSS, HTML, and JavaScript, with each project utilizing different technologies depending on the subject it was created for."
+      />
+      <Project 
+        projectTitle="University page" 
+        languages={projectUni}
+        image = {imageUni}
+        projectDescription = "This project is meant to be a 'folder' that contains all the projects that I've done along my university. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non turpis luctus, pretium nisl a, finibus erat. Aliquam vitae mattis massa, eu varius augue. Praesent nec condimentum dui. Phasellus vehicula sed nibh at suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam tempor, leo a pulvinar finibus, nunc neque ultrices neque, quis congue felis felis eget risus. Mauris scelerisque magna ac tristique porttitor. Sed varius mattis eros, sed luctus nunc consequat eu."
+      />
         
       </div>
     </div>
