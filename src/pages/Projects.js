7a721d783dee885fcce83ff10bccf7e889c6f6bf
Beellee <halley1986n@gmail.com>
2024-05-26 15:23:35 +0200

    refactor

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,18 +3,18 @@ import Project from "../components/ProjectDisplay";
 import '../styles/projects.css';
 
 //imagenes de los proyectos
-import imageUni from "../images/uniPage.png"
-import imageDataVisDash from "../images/dataVisDashboard.png"
-import imageProjectsWebsite from "../images/projectsWebsite.png"
+import imageCrypto from "../images/cryptoDM.jpeg"
+import imageHawksClassification from "../images/hawksClassification.jpeg"
+import imageProjectsWebsite from "../images/uniDB.jpeg"
 
 
 function UniProjects() {
   const scrollContainerRef = useRef(null);
 
   // Languages of the projects
-  const projectUni = ['JavaScript', 'HTML', 'CSS'];
+  const cryptocurrenciesDataMining = ['Python', 'ETL', 'APIs', 'Machine Learning'];
   const projectsWebsite =['React', 'JavaScript', 'HTML', 'CSS']
-  const dataVisDashboard = ['Pyhton', 'JavaScript', 'HTML', 'CSS']
+  const hawksClassification = ['R Software', 'Data Preparation', 'Data Analysis']
 
   useEffect(() => {
     const scrollContainer = scrollContainerRef.current;
@@ -41,26 +41,27 @@ function UniProjects() {
       </div>
       <div className='projectsWrapper'>
       <Project 
-        projectLink = "https://beellee.github.io/super-acorn-website/university"
-        projectTitle="University page" 
-        languages={projectUni}
-        image = {imageUni}
-        projectDescription = "This project serves as a digital repository for all my university projects. The website is built using CSS, HTML, and JavaScript, with each project utilizing different technologies depending on the subject it was created for."
+        projectLink = "https://github.com/Beellee/Data-Mining-project-on-Cryptocurrencies"
+        projectTitle="Data Mining project on Cryptocurrencies" 
+        languages={cryptocurrenciesDataMining}
+        image = {imageCrypto}
+        projectDescription = "In this data mining project, we analyze the historical price trends of Bitcoin, Ethereum, Solana, and BNB to gain insights into their price movements and patterns. The goal is also to predict whether a specific coin's price will rise or fall."
+      />
+      <Project 
+        projectTitle="Hawks Classification Analysis"
+        projectLink = "https://github.com/Beellee/hawks-classification-analysis" 
+        languages={hawksClassification}
+        image = {imageHawksClassification}
+        projectDescription = " This project aims to classify different species of hawks based on various physical and behavioral attributes. The primary goal is to develop a robust classification model that accurately identifies hawk species."
       />
       <div className='comingSoon'>
         <h3>Coming Soon ...</h3>
       </div>
       <Project 
-        projectTitle="Data visualization dashboard" 
-        languages={dataVisDashboard}
-        image = {imageDataVisDash}
-        projectDescription = " Interactive Data Visualization Dashboard showcasing global energy access, renewable energy adoption, and sustainability insights from 2000 to 2020. This project will allow the user to explore trends, economic impact, and geographical patterns for informed decision-making"
-      />
-      <Project 
         projectTitle="Project library" 
         languages={projectsWebsite}
         image = {imageProjectsWebsite}
-        projectDescription = "An organized online portfolio showcasing my university projects, sorted by technology for easy access and a seamless browsing experience."
+        projectDescription = "An organized online portfolio showcasing my university projects, sorted by technology for easy access and easy browsing experience."
       />
         
       </div>
